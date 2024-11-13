894d5683706b2c7e452664d395cdff408cf4e001
Кусакин Кирилл Святославович <22066@iat.iat>
2024-11-13 11:45:57 +0800

    123

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// src/App.js
+import React, { Suspense } from 'react';
+
+const PostsList = React.lazy(() => import('./components/PostsList'));
+
+function App() {
+  return (
+    <div>
+      <h1>Получение данных с API</h1>
+      <Suspense fallback={<p>Загрузка компонента...</p>}>
+        <PostsList />
+      </Suspense>
+    </div>
+  );
+}
+
+export default App;
